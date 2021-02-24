6608575e7c37911bf77e13b188bddc1d679e0841
fvarrui <4354486+fvarrui@users.noreply.github.com>
2021-02-24 12:48:32 +0000

    Update VideoJuego.java

--- src/main/java/dad/javafx/bindings/model/VideoJuego.java
+++ src/main/java/dad/javafx/bindings/model/VideoJuego.java
@@ -9,7 +9,7 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
-public class Videojuego {
+public class VideoJuego {
 
 	private StringProperty nombre = new SimpleStringProperty();
 	private StringProperty descripcion = new SimpleStringProperty();
